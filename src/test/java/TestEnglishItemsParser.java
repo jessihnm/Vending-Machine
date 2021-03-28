1ad4d0bcc5c31ee9cb933670715ed201eb599d0d
Jessica Heinemann <jessicah74@ymail.com>
2021-03-28 20:25:03 +0200

    renaming phone names to drink names

--- src/test/java/TestEnglishItemsParser.java
+++ src/test/java/TestEnglishItemsParser.java
@@ -19,8 +19,8 @@ public class TestEnglishItemsParser {
 
     @Test
     public void parseSingleItemChangesState() {
-        //Given the String "1 iPhone."
-        String data = "1 iPhone.";
+        //Given the String "1 Pepsi."
+        String data = "1 Pepsi.";
         //And and instance of EnglishItemsParser
         EnglishItemsParser parser = new EnglishItemsParser();
         //When I call parse
@@ -31,58 +31,79 @@ public class TestEnglishItemsParser {
 
     @Test
     public void parseSingleItemReturnsHashMapWithItem() {
-        //Given the String "3 iPhone."
-        String data = "3 iPhone.";
+        //Given the String "3 Pepsi."
+        String data = "3 Pepsi.";
         //And and instance of EnglishItemsParser
         EnglishItemsParser parser = new EnglishItemsParser();
         //When I call parse
         Map<String, Integer> items = parser.parse(data);
         //Then it should have one item
         assertEquals(1, items.size());
-        //And it should have an iPhone
+        //And it should have an Pepsi
         assertEquals(items, new HashMap<String, Integer>()
         {{
             // https://stackoverflow.com/questions/8261075/adding-multiple-entries-to-a-hashmap-at-once-in-one-statement
-            put("iPhone", 3);
+            put("Pepsi", 3);
         }});
         //Then it should be in the state END
         assertEquals(EnglishParsingState.END, parser.getCurrentState());
     }
     @Test
     public void parseSingleItemVariation2() {
-        //Given the String "2 Samsung."
-        String data = "2 Samsung.";
+        //Given the String "2 Sprite."
+        String data = "2 Sprite.";
         //And and instance of EnglishItemsParser
         EnglishItemsParser parser = new EnglishItemsParser();
         //When I call parse
         Map<String, Integer> items = parser.parse(data);
         //Then it should have one item
         assertEquals(1, items.size());
-        //And it should have a Samsung
+        //And it should have a Sprite
         assertEquals(items, new HashMap<String, Integer>()
         {{
             // https://stackoverflow.com/questions/8261075/adding-multiple-entries-to-a-hashmap-at-once-in-one-statement
-            put("Samsung", 2);
+            put("Sprite", 2);
         }});
         //Then it should be in the state END
         assertEquals(EnglishParsingState.END, parser.getCurrentState());
     }
     @Test
     public void parseTwoItemsReturnHashmapWithThem() {
-        //Given a string with 2 items (comma separated) "2 iPhone, 4 Samsung."
-        String data = "2 iPhone, 4 Samsung.";
+        //Given a string with 2 items (comma separated) "2 Pepsi, 4 Sprite."
+        String data = "2 Pepsi, 4 Sprite.";
         //And and instance of EnglishItemsParser
         EnglishItemsParser parser = new EnglishItemsParser();
         //When I call parse
         Map<String, Integer> items = parser.parse(data);
         //Then it should have two items
         assertEquals(2, items.size());
-        //And it should have 2 iphones and 4 samsungs
+        //And it should have 2 Pepsis and 4 Sprites
         assertEquals(items, new HashMap<String, Integer>()
         {{
             // https://stackoverflow.com/questions/8261075/adding-multiple-entries-to-a-hashmap-at-once-in-one-statement
-            put("iPhone", 2);
-            put("Samsung", 4);
+            put("Pepsi", 2);
+            put("Sprite", 4);
+        }});
+        //Then it should be in the state END
+        assertEquals(EnglishParsingState.END, parser.getCurrentState());
+    }
+    @Test
+    public void parseThreeItemsReturnHashmapWithThem() {
+        //Given a string with 3 items "2 Pepsi, 4 Sprite, 3 Fanta."
+        String data = "2 Pepsi, 4 Sprite, 3 Fanta.";
+        //And and instance of EnglishItemsParser
+        EnglishItemsParser parser = new EnglishItemsParser();
+        //When I call parse
+        Map<String, Integer> items = parser.parse(data);
+        //Then it should have three items
+        assertEquals(3, items.size());
+        //And it should have 2 Pepsis, 4 Sprites and 3 Fantas
+        assertEquals(items, new HashMap<String, Integer>()
+        {{
+            // https://stackoverflow.com/questions/8261075/adding-multiple-entries-to-a-hashmap-at-once-in-one-statement
+            put("Pepsi", 2);
+            put("Sprite", 4);
+            put("Fanta", 3);
         }});
         //Then it should be in the state END
         assertEquals(EnglishParsingState.END, parser.getCurrentState());
